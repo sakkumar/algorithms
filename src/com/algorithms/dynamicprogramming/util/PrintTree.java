6817816b6a57e60fa8942145a3ffea58fc01ea49
sakkumar <saket.kumar29@gmail.com>
2019-12-29 12:26:03 -0800

    Print binary tree in system console

---
+++ src/com/algorithms/dynamicprogramming/util/PrintTree.java
@@ -0,0 +1,143 @@
+package com.algorithms.dynamicprogramming.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Print a tree in console for better visualization
+ *        2
+ *       / \
+ *      /   \
+ *     /     \
+ *    /       \
+ *    7       5
+ *   / \       \
+ *  /   \       \
+ *  2   6       9
+ *     / \     /
+ *     5 8     4
+ *
+ *
+ */
+class Node<T extends Comparable<?>> {
+    Node<T> left, right;
+    T data;
+
+    public Node(T data) {
+        this.data = data;
+    }
+}
+
+class BTreePrinter {
+
+    public static <T extends Comparable<?>> void printNode(Node<T> root) {
+        int maxLevel = BTreePrinter.maxLevel(root);
+
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static <T extends Comparable<?>> void printNodeInternal(List<Node<T>> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || BTreePrinter.isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        BTreePrinter.printWhitespaces(firstSpaces);
+
+        List<Node<T>> newNodes = new ArrayList<Node<T>>();
+        for (Node<T> node : nodes) {
+            if (node != null) {
+                System.out.print(node.data);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            BTreePrinter.printWhitespaces(betweenSpaces);
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                BTreePrinter.printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    BTreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).left != null)
+                    System.out.print("/");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).right != null)
+                    System.out.print("\\");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println("");
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+
+    private static <T extends Comparable<?>> int maxLevel(Node<T> node) {
+        if (node == null)
+            return 0;
+
+        return Math.max(BTreePrinter.maxLevel(node.left), BTreePrinter.maxLevel(node.right)) + 1;
+    }
+
+    private static <T> boolean isAllElementsNull(List<T> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Node<Integer> root = new Node<Integer>(2);
+        Node<Integer> n11 = new Node<Integer>(7);
+        Node<Integer> n12 = new Node<Integer>(5);
+        Node<Integer> n21 = new Node<Integer>(2);
+        Node<Integer> n22 = new Node<Integer>(6);
+        Node<Integer> n23 = new Node<Integer>(9);
+        Node<Integer> n31 = new Node<Integer>(5);
+        Node<Integer> n32 = new Node<Integer>(8);
+        Node<Integer> n33 = new Node<Integer>(4);
+
+        root.left = n11;
+        root.right = n12;
+
+        n11.left = n21;
+        n11.right = n22;
+
+        n12.right = n23;
+        n22.left = n31;
+        n22.right = n32;
+
+        n23.left = n33;
+
+        BTreePrinter.printNode(root);
+    }
+
+}
